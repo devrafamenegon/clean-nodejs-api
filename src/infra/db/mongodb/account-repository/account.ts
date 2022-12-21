0a9bc1daea7a7e3c793815e93e3183dd65bfc480
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2022-12-21 16:28:25 -0300

    refactor: move sut creation to a factory helper method

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -3,14 +3,19 @@ import { AccountModel } from '../../../../domain/models/account'
 import { AddAccountModel } from '../../../../domain/usecases/add-account'
 import { MongoHelper } from '../helpers/mongo-helper'
 
+interface MongoAccountModel {
+  _id: string
+  name: string
+  email: string
+  password: string
+}
+
 export class AccountMongoRepository implements AddAccountRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts')
-    const result = await accountCollection.insertOne(accountData)
-    const account = result.insertedId
-    const addedAccount = await accountCollection.findOne(account)
-    const { _id, ...accountWithoutId } = addedAccount as any
-
+    const { insertedId } = await accountCollection.insertOne(accountData)
+    const addedAccount = await accountCollection.findOne(insertedId) as unknown as MongoAccountModel
+    const { _id, ...accountWithoutId } = addedAccount
     return Object.assign({}, accountWithoutId, { id: _id }) as AccountModel
   }
 }
